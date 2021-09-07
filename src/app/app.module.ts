23c39a33aee00cc1263ad163f68863d59bf1d13c
Vincent DEVAUX <vincent.devaux@edu.itescia.fr>
2021-09-07 15:34:27 +0200

    Add article page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { HttpClientModule } from '@angular/common/http';
 import { CreationaccountComponent } from './creationaccount/creationaccount.component';
+
+import { ArticleComponent } from './article/article.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreationaccountComponent
+    CreationaccountComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
